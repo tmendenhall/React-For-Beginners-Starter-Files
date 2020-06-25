5041add575404bdd02a5453ee61074ac4c9efa08
Tyler Mendenhall <tyler.mendenhall@e-gineering.com>
2020-06-25 09:02:13 -0400

    header is now a stateless functional component

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,23 +1,22 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="top">
-                <h3 className="tagline">
-                    <span>Fresh Daily</span>
-                </h3>
-                <h1>
-                    Catch
-                    <span className="ofThe">
+const Header = ({tagline}) => (
+    // return (
+        <header className="top">
+            <h1>
+                Catch
+                <span className="ofThe">
                         <span className="of">of</span>
                         <span className="the">the</span>
                      </span>
-                    Day
-                </h1>
-            </header>
-        )
-    }
-}
+                Day
+            </h1>
+            <h3 className="tagline">
+                <span>{tagline}</span>
+            </h3>
+        </header>
+    // )
+)
+
 
 export default Header;
