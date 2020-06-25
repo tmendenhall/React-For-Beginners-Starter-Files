cf5a931147098d7bdda0d41e6955bc3c5cfb6717
Tyler Mendenhall <tyler.mendenhall@e-gineering.com>
2020-06-25 15:21:01 -0400

    add state updates based on top level app object.. and functions pushed down

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,20 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component {
+    state = {
+      fishes: {},
+      order: {}
+    };
+    addFish = fish => {
+        // 1. copy the existing state
+        const fishes = {...this.state.fishes}; // object spread to copy an object
+        // 2. add new fish to fishes
+        fishes[`fish${Date.now()}`] = fish;
+        this.setState({
+            fishes  // fishes: fishes
+        }); // object to update and new data
+
+    }
     render() {
         return (
           <div className="catch-of-the-day">
@@ -11,7 +25,7 @@ class App extends React.Component {
                   <Header tagline="Fresh Seafood Market"/>
               </div>
               <Order/>
-              <Inventory/>
+              <Inventory addFish = {this.addFish}/>
           </div>
         );
     }
