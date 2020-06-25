0192a51599c4df1de1358067cf1a08f4d1594769
Tyler Mendenhall <tyler.mendenhall@e-gineering.com>
2020-06-25 14:12:01 -0400

    fix not in the video input.current.value not input.value.value

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,23 @@
 import React, {Fragment} from "react";
+import {getFunName} from "../helpers";
 
 class StorePicker extends React.Component {
-    render() {
 
+    myInput = React.createRef();
+
+    goToStore = event => { // creates a function in the context of the StorePicker component to access 'this'
+        event.preventDefault(); // stop form from submitting
+        // get text input
+        console.log(this.myInput.current.value);
+        // change the route
+    }
+
+    render() {
         return (
             <Fragment>
-                <form className="store-selector">
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>Please enter a Store</h2>
-                    <input type="text" required placeholder="Store Name"/>
+                    <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={this.myInput}/>
                     <button type="submit">Visit Store -></button>
                 </form>
             </Fragment>)
